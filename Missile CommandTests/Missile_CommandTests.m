f9620da8150fe6f7a4c6357a361e8a239d754d75
Jesus Magana <maganazero5@gmail.com>
2014-07-10 10:52:47 -0700

    Initial Commit

---
+++ Missile CommandTests/Missile_CommandTests.m
@@ -0,0 +1,34 @@
+//
+//  Missile_CommandTests.m
+//  Missile CommandTests
+//
+//  Created by Jesus Magana on 7/10/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Missile_CommandTests : XCTestCase
+
+@end
+
+@implementation Missile_CommandTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
