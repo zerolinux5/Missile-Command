f457e4406a0c7665829dceba757b094f6ec9b2f8
Jesus Magana <maganazero5@gmail.com>
2014-07-10 11:03:12 -0700

    Added buttons to transition to other scenes

--- Missile Command/MenuScene.m
+++ Missile Command/MenuScene.m
@@ -8,6 +8,17 @@
 
 #import "MenuScene.h"
 
+#import "MyScene.h"
+#import "MultiScene.h"
+
+@interface MenuScene () {
+    CGSize sizeGlobal;
+    UIButton *singlePlayerButton;
+    UIButton *multiPlayerButton;
+}
+
+@end
+
 @implementation MenuScene
 
 - (id)initWithSize:(CGSize)size {
@@ -21,9 +32,55 @@
         title.scale = 0.4;
         title.position = CGPointMake(size.width/2,size.height/2);
         [self addChild:title];
+        
+        sizeGlobal = size;
     }
     
     return self;
+}
+- (void)didMoveToView:(SKView *)view {
+    UIImage *buttonImageNormal = [UIImage imageNamed:@"singleBtn.png"];
+    singlePlayerButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    singlePlayerButton.frame = CGRectMake(sizeGlobal.height/8, sizeGlobal.width/2+250, buttonImageNormal.size.width, buttonImageNormal.size.height);
+    singlePlayerButton.backgroundColor = [UIColor clearColor];
+    [singlePlayerButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
+    UIImage *strechableButtonImageNormal = [buttonImageNormal stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [singlePlayerButton setBackgroundImage:strechableButtonImageNormal forState:UIControlStateNormal];
+    [singlePlayerButton addTarget:self action:@selector(moveToSinglePlayerGame) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:singlePlayerButton];
+    
+    UIImage *buttonImageNormal2 = [UIImage imageNamed:@"multiBtn.png"];
+    multiPlayerButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    multiPlayerButton.frame = CGRectMake(sizeGlobal.height/2+100, sizeGlobal.width/2+250, buttonImageNormal2.size.width, buttonImageNormal2.size.height);
+    multiPlayerButton.backgroundColor = [UIColor clearColor];
+    [multiPlayerButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
+    UIImage *strechableButtonImageNormal2 = [buttonImageNormal2 stretchableImageWithLeftCapWidth:12 topCapHeight:0];
+    [multiPlayerButton setBackgroundImage:strechableButtonImageNormal2 forState:UIControlStateNormal];
+    [multiPlayerButton addTarget:self action:@selector(moveToMultiPlayerGame) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:multiPlayerButton];
+}
+
+- (void)moveToSinglePlayerGame {
+    SKScene * scene = [MyScene sceneWithSize:self.view.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    SKTransition* transition = [SKTransition revealWithDirection:SKTransitionDirectionLeft duration:1];
+    SKView * skView = (SKView *)self.view;
+    [skView presentScene:scene transition:transition];
+    
+    [singlePlayerButton removeFromSuperview];
+    [multiPlayerButton removeFromSuperview];
+}
+- (void)moveToMultiPlayerGame {
+    SKScene * scene = [MultiScene sceneWithSize:self.view.bounds.size];
+    scene.scaleMode = SKSceneScaleModeAspectFill;
+    SKTransition* transition = [SKTransition revealWithDirection:SKTransitionDirectionLeft duration:1];
+    SKView * skView = (SKView *)self.view;
+    [skView presentScene:scene transition:transition];
+    
+    [singlePlayerButton removeFromSuperview];
+    [multiPlayerButton removeFromSuperview];
 }
 
 @end
